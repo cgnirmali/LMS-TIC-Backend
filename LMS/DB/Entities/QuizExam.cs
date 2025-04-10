67bded37c81c8f426eaf685cdb5ae54cb4f96a67
Your Name <youremail@example.com>
2025-04-10 17:19:35 +0530

    add name into quiz exam

--- LMS/DB/Entities/QuizExam.cs
+++ LMS/DB/Entities/QuizExam.cs
@@ -3,6 +3,7 @@
     public class QuizExam
     {
         public Guid Id { get; set; }
+        public string Name { get; set; }    
         public DateTime CreatedDate { get; set; }
         public string Description {  get; set; }
         public DateTime Duration { get; set; }
